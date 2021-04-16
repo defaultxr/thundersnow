496e8dfbef025c3a7da6279036283e20ce0094be
modula t. worm <defaultxr@gmail.com>
2021-04-16 01:24:15 -0500

    update utility.lisp

--- src/utility.lisp
+++ src/utility.lisp
@@ -1,15 +1,8 @@
 (in-package #:thundersnow/common)
 
-(defparameter *swank-output* *standard-output*)
-
-(defmacro with-swank-output (&body body)
-  "Run BODY with *standard-output* bound to the swank output (so `print'/etc don't print to the CLIM stream). Useful for debugging."
-  `(let ((*standard-output* *swank-output*))
-     ,@body))
-
 (defun sprint (object)
   "Swank print; sugar to print to the swank output, avoiding any CLIM interactors."
-  (print object *swank-output*))
+  (print object *debug-io*))
 
 (defun all-command-tables ()
   "Get a list of all defined CLIM command tables."
