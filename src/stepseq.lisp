debc6f57b82557fa106047080f1662b95783d6f4
modula t. worm <defaultxr@gmail.com>
2022-03-05 23:47:04 -0600

    minor changes to the stepseq stub

--- src/stepseq.lisp
+++ src/stepseq.lisp
@@ -1,13 +1,19 @@
 (in-package #:thundersnow/stepseq)
 
+(defvar *tmp* nil
+  "Temporary variable for convenience during development.")
+
 (defclass stepseq-pane (application-pane)
   ((pattern :initarg :pattern :initform nil :accessor pane-pattern))
-  (:default-initargs
-   :name 'stepseq
-   :display-function 'display-stepseq))
+  (:default-initargs :name 'stepseq
+                     :display-function 'display-stepseq))
 
 (defun display-stepseq (frame stream)
-  nil)
+  (declare (ignore frame))
+  (with-slots (pattern) stream
+    (unless pattern
+      (draw-text stream "nil" (bounding-rectangle-center stream))
+      (return-from display-stepseq))))
 
 (define-command-table stepseq-file-command-table
   :inherit-from (thundersnow-common-file-command-table)
@@ -42,44 +48,21 @@
                           ("View" :menu stepseq-view-command-table)
                           ("Tools" :menu stepseq-tools-command-table)
                           ("Help" :menu stepseq-help-command-table))))
-  (:default-initargs
-   :pretty-name "stepseq")
+  (:default-initargs :pretty-name "Stepseq")
   (:panes
-   ;; (raised (raising (:border-width 3 :background +Gray83+)
-   ;;           (make-pane 'check-box :choices '("First" "Second" "Third"))))
-   (logo :application
-         :scroll-bars nil
-         ;; :display-function
-         )
-   (tempo (make-pane 'tempo-pane :name 'tempo-pane))
-   (scope (make-pane 'scope-pane :name 'scope-pane))
    (stepseq-pane (make-pane 'stepseq-pane :name 'stepseq-pane))
-   ;; (input :text-editor)
-   ;; (pattern-view :application
-   ;;               :scroll-bars t
-   ;;               :display-function 'display-pattern)
    (interactor :interactor))
   (:layouts
    (default
     (raising (:border-width 0 :background +black+)
       (vertically ()
-        (1/10 (horizontally () ;; toolbar
-                (8/10 logo)
-                ;; FIX: add server status pane with cpu load, number of active ugens, active synths, active groups, and number of synthdefs
-                (1/10 tempo)
-                (1/10 scope)))
-        (7/10 stepseq-pane
-              ;; (labelling (:label "Options")
-              ;;   input)
-              )
+        (4/5 stepseq-pane)
         (make-pane 'clime:box-adjuster-gadget)
-        (2/10 interactor))))
+        (1/5 interactor))))
    (test-interactor
     (vertically ()
       interactor)))
-  (:pointer-documentation t)
-  ;; (:top-level (stepseq-frame-top-level . nil))
-  )
+  (:pointer-documentation t))
 
 (defun stepseq ()
   "Open a stepseq or get one of the instances already open.
